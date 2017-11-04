139772e3349cc7051b0b749990ed39b38d888b20
Denis Nishida <denis.nishida@gmail.com>
2017-11-03 22:00:09 -0500

    Added verification of the current location

--- app/src/main/java/com/example/winnipegbusschedules/MapsActivity.java
+++ app/src/main/java/com/example/winnipegbusschedules/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.winnipegbusschedules;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -9,20 +10,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity
         implements GoogleMap.OnMyLocationButtonClickListener,
-                   OnMapReadyCallback
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener
 {
   final int MY_LOCATION_REQUEST_CODE = 1;
 
   private GoogleMap mMap;
+  private GoogleApiClient mGoogleApiClient;
+  private Location mLastLocation;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -33,6 +40,16 @@ public class MapsActivity extends AppCompatActivity
     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
             .findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
+
+    // Create an instance of GoogleAPIClient.
+    if (mGoogleApiClient == null)
+    {
+      mGoogleApiClient = new GoogleApiClient.Builder(this)
+              .addConnectionCallbacks(this)
+              .addOnConnectionFailedListener(this)
+              .addApi(LocationServices.API)
+              .build();
+    }
   }
 
 
@@ -64,11 +81,6 @@ public class MapsActivity extends AppCompatActivity
       ActivityCompat.requestPermissions(MapsActivity.this, new String[]
               {android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
     }
-
-    // Add a marker in Sydney and move the camera
-    LatLng sydney = new LatLng(-34, 151);
-    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
   }
 
   // Event Handler when the location button is clicked
@@ -110,5 +122,57 @@ public class MapsActivity extends AppCompatActivity
                 Toast.LENGTH_LONG).show();
       }
     }
+  }
+
+  @Override
+  protected void onStart()
+  {
+    mGoogleApiClient.connect();
+    super.onStart();
+  }
+
+  @Override
+  protected void onStop()
+  {
+    mGoogleApiClient.disconnect();
+    super.onStop();
+  }
+
+  @Override
+  public void onConnected(Bundle connectionHint)
+  {
+    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+    {
+      // TODO: Consider calling
+      //    ActivityCompat#requestPermissions
+      // here to request the missing permissions, and then overriding
+      //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+      //                                          int[] grantResults)
+      // to handle the case where the user grants the permission. See the documentation
+      // for ActivityCompat#requestPermissions for more details.
+      return;
+    }
+
+    mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+    if (mLastLocation != null)
+    {
+      LatLng currentLocation = new LatLng(mLastLocation.getLatitude(),
+              mLastLocation.getLongitude());
+
+      mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 17));
+    }
+  }
+
+  @Override
+  public void onConnectionSuspended(int i)
+  {
+
+  }
+
+  @Override
+  public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+  {
+
   }
 }
