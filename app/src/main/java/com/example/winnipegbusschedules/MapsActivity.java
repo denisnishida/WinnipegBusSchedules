d2840c8ffde028c46c4c8e222cdd36705ac1bb02
Denis Nishida <denis.nishida@gmail.com>
2017-11-04 20:54:16 -0500

    Get current location and move the map there

--- app/src/main/java/com/example/winnipegbusschedules/MapsActivity.java
+++ app/src/main/java/com/example/winnipegbusschedules/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.winnipegbusschedules;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -19,6 +23,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MapsActivity extends AppCompatActivity
         implements GoogleMap.OnMyLocationButtonClickListener,
         OnMapReadyCallback,
@@ -27,9 +43,18 @@ public class MapsActivity extends AppCompatActivity
 {
   final int MY_LOCATION_REQUEST_CODE = 1;
 
+  final String API_KEY = "?api-key=rQ8lXW4lpLR9CwiYqK";
+  final String BEGIN_URL = "https://api.winnipegtransit.com/v2/";
+  final String JSON_APPEND = ".json";
+  final String STATUS_SCHEDULE_REQUEST = "statuses/schedule";
+  final String STOP_SCHEDULE_REQUEST_BEGIN = "stops/";
+  final String STOP_SCHEDULE_REQUEST_END = "/schedule";
+
   private GoogleMap mMap;
   private GoogleApiClient mGoogleApiClient;
   private Location mLastLocation;
+
+  String requestUrl;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -71,7 +96,6 @@ public class MapsActivity extends AppCompatActivity
     if (ContextCompat.checkSelfPermission(this,
             android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
     {
-      Log.d("Jody", "enabling my location");
       mMap.setMyLocationEnabled(true);
       mMap.setOnMyLocationButtonClickListener(this);
     }
@@ -157,10 +181,19 @@ public class MapsActivity extends AppCompatActivity
 
     if (mLastLocation != null)
     {
-      LatLng currentLocation = new LatLng(mLastLocation.getLatitude(),
-              mLastLocation.getLongitude());
+      double lat = mLastLocation.getLatitude();
+      double lon = mLastLocation.getLongitude();
+
+      LatLng currentLocation = new LatLng(lat, lon);
 
       mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 17));
+
+      // URL to request the stops
+      // ttp://api.winnipegtransit.com/v2/stops.json?distance=500&lat=49.895&lon=-97.138&api-key=rQ8lXW4lpLR9CwiYqK
+      requestUrl = BEGIN_URL + STOP_SCHEDULE_REQUEST_BEGIN + JSON_APPEND
+                   + "?distance=500" + "&lat=" + lat + "&lon=" + lon + API_KEY;
+
+      processRequest();
     }
   }
 
@@ -174,5 +207,143 @@ public class MapsActivity extends AppCompatActivity
   public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
   {
 
+  }
+
+  private boolean isNetworkAvailable()
+  {
+    ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+  }
+
+  public void processRequest()
+  {
+    if (isNetworkAvailable())
+    {
+      // create and execute AsyncTask
+      ProcessingTask task = new ProcessingTask();
+      task.execute();
+    }
+    else
+    {
+      Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG).show();
+    }
+  }
+
+  class ProcessingTask extends AsyncTask
+  {
+    // On the start of the thread
+    @Override
+    protected void onPreExecute()
+    {
+      super.onPreExecute();
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects)
+    {
+      // Create URL object to RSS file
+      URL url = null;
+
+      try
+      {
+        url = new URL(requestUrl);
+      }
+      catch (MalformedURLException e)
+      {
+        e.printStackTrace();
+      }
+
+      // Create and open HTTP connection
+      HttpURLConnection connection = null;
+      try
+      {
+        connection = (HttpURLConnection) url.openConnection();
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+
+      try
+      {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null)
+        {
+          stringBuilder.append(line);
+        }
+
+        bufferedReader.close();
+
+        return stringBuilder.toString();
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+      finally
+      {
+        connection.disconnect();
+      }
+
+      return null;
+    }
+
+    // On the end of the thread
+    @Override
+    protected void onPostExecute(Object o)
+    {
+      if (o != null)
+      {
+        String response = o.toString();
+
+        Log.i("INFO", o.toString());
+
+        parseJSON(response);
+      }
+      else
+      {
+        Toast.makeText(MapsActivity.this,
+                "Sorry, it was not possible to get the schedule information",
+                Toast.LENGTH_SHORT).show();
+      }
+    }
+
+    // Verify the request and get the values
+    private void parseJSON(String response)
+    {
+      try
+      {
+        // Using orj.json, get the file string and convert it to an object
+        JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+
+        // The Winnipeg Transit JSON results usually have nested values
+        // We can identify the request by the first key of the first level
+
+        // The method names() will retrieve an JSONArray with the key names
+        JSONArray objectNames = object.names();
+
+        // Retrieve the first key of the first level
+        String firstKey = objectNames.getString(0);
+
+        if (firstKey.equals("stops"))
+        {
+          extractStops(object.getJSONArray(firstKey));
+        }
+      }
+      catch (JSONException e)
+      {
+        e.printStackTrace();
+      }
+    }
+
+    // Get the information from the status request
+    private void extractStops(JSONArray stopsArray) throws JSONException
+    {
+      //String message = statusObject.getString("message");
+    }
   }
 }
