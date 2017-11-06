955a6468f5fe664459be1e5b7665866e52002016
Denis Nishida <denis.nishida@gmail.com>
2017-11-06 09:03:03 -0600

    Created and designed StopActivity, strings are not working?

---
+++ app/src/main/java/com/example/winnipegbusschedules/StopActivity.java
@@ -0,0 +1,15 @@
+package com.example.winnipegbusschedules;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StopActivity extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_stop);
+  }
+}
